f3fd2f25b7f75a48344ec36c2d3850d339c4d6d5
vladislav-ivanov <vladislav.ivanov90@gmail.com>
2019-04-10 14:54:03 +0200

    new

---
+++ Arrays/condenseArrayToNumber.js
@@ -0,0 +1,12 @@
+function solve(nums){
+let condensed =[];
+while(nums.length > 1){
+    for(let i = 0; i < nums.length-1; i++){
+        condensed.push(nums[i] + nums[i+1])
+    }
+    nums = condensed.slice();
+    condensed = [];
+}
+console.log(nums[0]);
+}
+solve([2,10,3]);
